e8caae9746350a607a40417c5466c999a120e81f
satish.voddi <satish.voddi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-18 21:38:45 +0000

    Updating
    git-svn-id: https://plugins.svn.wordpress.org/hyperlinkpopup/trunk@334336 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/linkspopup.js
+++ js/linkspopup.js
@@ -7,32 +7,19 @@
 				modal:true,
 				title:'title',
 				resizable:false,
-				draggable:false
+				draggable:false,
+				background:'transparent url(http://img227.imageshack.us/img227/6100/pageloading.gif) no-repeat'
 		}
 
-		var pop_frame = $("<iframe id='pop_frame' frameborder='0'></iframe>");
+		var pop_frame = $("<object id='pop_frame' data='' type='text/html' frameborder='0'></object>");
 		var style_frame = {
-			border:'none'	
+			border:'none'
 		}
 		pop_frame.css(style_frame);
 
 		dialog.append(pop_frame);
 		$('body').append(dialog);
 
-		var loading_div = $("<div id='loading_div'></div>");
-		var loading_img = new Image();
-		loading_img.src = "http://img227.imageshack.us/img227/6100/pageloading.gif";
-		var style_img = {
-			position:'fixed',
-			left:'50%',
-			marginTop:'-52px',
-			marginLeft:'-95px',
-			display:'none'
-		}
-		loading_div.append(loading_img);
-		loading_div.css(style_img);
-		$('body').append(loading_div);
-		
 		var win_ht = $(window).height();
 		var win_wd = $(window).width();
 
@@ -45,32 +32,22 @@
 		$('#pop_frame').css('height',pop_ht-35+'px');
 		$('#pop_frame').css('width',pop_wd-45+'px');
 
-		var pop_src='';
-		var title = '';
-		pop_frame.load(function(e){
-			if(pop_src.length>0){
-				$('#dialog').dialog('open');
-				$('#dialog').dialog({title:title});
-			}
-			loading_div.hide();
-		});
-
 		$('a[rel*=pop]').live('click',function(){
 			pop_src = $(this).attr('href');
 			title = $(this).attr('title');
 			if(title.length == 0){
 				title = pop_src;
 			}
-			pop_frame.attr('src',pop_src);
-			loading_div.css('top',$(window).height()/2+'px');
-			loading_div.show();
+			pop_frame.attr('data',pop_src);
+			$('#dialog').dialog('open');
+			$('#dialog').dialog('option','title',title);
 			return false;
 		});
 		
 		$('#dialog').dialog({
 			close:function(event,ui){
 			pop_src='';
-				pop_frame.attr('src',pop_src);
+				pop_frame.attr('data',pop_src);
 			}
 		});
   };
