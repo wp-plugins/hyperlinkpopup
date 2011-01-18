4b788873c41aae2d8da95f686c78a68eb4f42942
satish.voddi <satish.voddi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-18 17:41:10 +0000

    Updating
    git-svn-id: https://plugins.svn.wordpress.org/hyperlinkpopup/trunk@334232 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hyperlinkPopup.php
+++ hyperlinkPopup.php
@@ -12,6 +12,6 @@ function addHeaderCode() {
 	echo '<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>' . "\n";   
 	echo '<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.8/jquery-ui.min.js"></script>' . "\n";
 	echo "<link href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css' rel='stylesheet' type='text/css'/>"."\n";  
-	echo '<script type="text/javascript" src="'.get_bloginfo('wpurl').'/wp-content/plugins/HyperlinkPopup/js/linkspopup.js"></script>' . "\n";   
+	echo '<script type="text/javascript" src="http://plugins.svn.wordpress.org/hyperlinkpopup/trunk/js/linkspopup.js"></script>' . "\n";   
 }
 ?>
