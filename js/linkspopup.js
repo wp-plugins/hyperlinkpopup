a3690f68914dc1a1287cde666d0a591d2a19013b
satish.voddi <satish.voddi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-26 19:15:41 +0000

    updating minor fixes in plugin for IE
    git-svn-id: https://plugins.svn.wordpress.org/hyperlinkpopup/trunk@337553 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/linkspopup.js
+++ js/linkspopup.js
@@ -11,28 +11,30 @@
 				background:'transparent url(http://img227.imageshack.us/img227/6100/pageloading.gif) no-repeat'
 		}
 
+		/*
 		var pop_frame = $("<object id='pop_frame' data='' type='text/html'></object>");
 		var style_frame = {
 			border:'none'
 		}
 		pop_frame.css(style_frame);
-
-		dialog.append(pop_frame);
+		//dialog.append(pop_frame);
+		*/
 		$('body').append(dialog);
 
 		var win_ht = $(window).height();
 		var win_wd = $(window).width();
 
-		var pop_ht = parseInt(3*win_ht/4)-70;
-		var pop_wd = parseInt(3*win_wd/4)-70;
+		var pop_ht = 3*win_ht/4;
+		var pop_wd = 3*win_wd/4;
 		
 		$("#dialog").dialog( dialog_options );
 		$('#dialog').dialog({minHeight:pop_ht, minWidth:pop_wd});
 
-		$('#pop_frame').css('height',pop_ht+'px');
-		$('#pop_frame').css('width',pop_wd+'px');
+		/*
+		$('#pop_frame').css('height',pop_ht-35+'px');
+		$('#pop_frame').css('width',pop_wd-45+'px');
+		*/
 
-		var src = "wp-content/plugins/hyperlinkPopup2/openthis.html";
 		$('a[rel*=pop]').live('click',function(e){
 			e.preventDefault();
 			pop_src = $(this).attr('href');
@@ -40,7 +42,13 @@
 			if(title.length == 0){
 				title = pop_src;
 			}
-			pop_frame.attr('data',src+'?url='+pop_src+'&size='+pop_ht+','+pop_wd);
+		    var obj = document.createElement("object");
+		    obj.data = pop_src;
+		    obj.type = "text/html";
+		    obj.id = "obj";
+		    obj.width = pop_wd-45+'px';
+		    obj.height = pop_ht-35+'px';
+		    document.getElementById('dialog').appendChild(obj);
 			$('#dialog').dialog('open');
 			$('#dialog').dialog('option','title',title);
 			return false;
@@ -48,8 +56,11 @@
 		
 		$('#dialog').dialog({
 			close:function(event,ui){
-			pop_src='';
+				/*
+				pop_src='';
 				pop_frame.attr('data',pop_src);
+				*/
+				document.getElementById('dialog').removeChild( document.getElementById('obj') );
 			}
 		});
   };
